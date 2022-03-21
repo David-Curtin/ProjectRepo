63b8cb165a76368cd924ac08711e1d4626f32aea
David Curtin <L00169721@student.lyit.ie>
2022-03-21 22:44:19 +0000

    login and logout overlayed, since they both wont be visible. logiv to be implemented later to only see one at a time

--- foodwasteapp/src/components/Navbar.js
+++ foodwasteapp/src/components/Navbar.js
@@ -1,28 +1,22 @@
 // imports
 import { Link } from "react-router-dom"
+import { useLogout } from '../hooks/useLogout'
 
 //styles
 import './Navbar.css'
 
 export default function Navbar() {
+  const { logout } = useLogout()
+
   return (
     <div className="navbar">
         <nav>
-        <Link to="/" className="home">
-            <h1>Home</h1>
-        </Link>
-        <Link to="/Favourites" className="home">
-            <h1>Favourites</h1>
-        </Link>
-        <Link to="/Search" className="home">
-            <h1>Search</h1>
-        </Link>
-        <Link to="/Login" className="login">
-            <h1>Login</h1>
-        </Link>
-        <Link to="/SignUp">
-            <h1>Sign Up</h1>
-        </Link>
+        <Link to="/" className="home"><h1>Home</h1></Link>
+        <Link to="/Favourites" className="home"><h1>Favourites</h1></Link>
+        <Link to="/Search" className="home"><h1>Search</h1></Link>
+        <Link to="/Login" className="login"><h1>Login</h1></Link>
+        <button className="button" onClick={logout}>Logout</button>
+        <Link to="/SignUp"> <h1>Sign Up</h1></Link>
         </nav>
     </div>
   )
