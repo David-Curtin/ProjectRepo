606f5cca6d0505374b82ea83199930b05621ec10
David Curtin <L00169721@student.lyit.ie>
2022-03-21 22:05:21 +0000

    git status /

--- foodwasteapp/src/hooks/useSignup.js
+++ foodwasteapp/src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { projectAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext"
 
 export const useSignup = () => {
     const [error, setError] = useState(null)
     const [isPending, setIsPending] = useState(false)
+    const { dispatch } = useAuthContext()
 
     const signup = async (email, password, displayName) => {
         setError(null)
@@ -11,13 +13,15 @@ export const useSignup = () => {
 
         try{
             const res = await projectAuth.createUserWithEmailAndPassword(email, password)
-            console.log(res.user)
 
             if (!res) {
                 throw new Error('Could not sign up')
             }
 
             await res.user.updateProfile({ displayName })
+
+            // dispathes login function
+            dispatch( {type: 'LOGIN', payload: res.user })
 
             setIsPending(false)
             setError(null)
