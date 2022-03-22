953bf6bbe621bceacac7c64db3572f97bc74d634
David Curtin <L00169721@student.lyit.ie>
2022-03-22 22:23:07 +0000

    Navbar changed to only show links depending on login state

--- foodwasteapp/src/components/Navbar.js
+++ foodwasteapp/src/components/Navbar.js
@@ -1,23 +1,36 @@
 // imports
 import { Link } from "react-router-dom"
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from "../hooks/useAuthContext"
 
 //styles
-import './Navbar.css'
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   return (
-    <div className="navbar">
-        <nav>
-        <Link to="/" className="home"><h1>Home</h1></Link>
-        <Link to="/Favourites" className="home"><h1>Favourites</h1></Link>
-        <Link to="/Search" className="home"><h1>Search</h1></Link>
-        <Link to="/Login" className="login"><h1>Login</h1></Link>
-        <button className="btn" onClick={logout}>Logout</button>
-        <Link to="/SignUp"> <h1>Sign Up</h1></Link>
+    // <div >
+        <nav className={styles.navbar}>
+          <ul>
+            {!user && (    
+              <>
+                <li><Link to="/Login" className="login"><h1>Login</h1></Link></li>
+                <li><Link to="/SignUp"> <h1>Sign Up</h1></Link></li>
+              </>
+            )}
+
+            {user && (
+              <>
+                <li><Link to="/" className="home"><h1>Home</h1></Link></li>
+                <li><Link to="/Favourites" className="home"><h1>Favourites</h1></Link></li>
+                <li><Link to="/Search" className="home"><h1>Search</h1></Link></li>
+                <button className="btn" onClick={logout}>Logout</button>
+              </>
+            )}
+          </ul>
         </nav>
-    </div>
+    // </div>
   )
 }
