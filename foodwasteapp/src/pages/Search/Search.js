d0587d89a6c215dbf4ef0bf69d0892766b5298a0
David Curtin <L00169721@student.lyit.ie>
2022-03-29 12:48:36 +0100

    search bar added to search page, including useEffect

--- foodwasteapp/src/pages/Search/Search.js
+++ foodwasteapp/src/pages/Search/Search.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+import { Button } from "bootstrap"
+
 export default function Search() {
+
+  const baseUrl = 'https://api.spoonacular.com/recipes/'
+  const apiKey = 'apiKey=6375070d85b04e8eae28ab8097147047'
+  const exampleTestedRequest = 'https://api.spoonacular.com/recipes/716429/information?apiKey=6375070d85b04e8eae28ab8097147047&includeNutrition=true.'
+  const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+      console.log('effect used')
+    }, [])
+
     return (
+
       <div>
-          <h1>Search</h1>
+          <form className="search-form">
+            <input className="search-bar" type="text"/>
+            <button class="btn btn-primary" type="submit">Search</button>
+          </form>
       </div>
     )
   }
