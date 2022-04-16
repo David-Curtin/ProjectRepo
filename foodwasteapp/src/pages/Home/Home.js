3a379551036a1d1a2edc85ac568ae3c4fbb543a2
David Curtin <L00169721@student.lyit.ie>
2022-04-16 15:01:14 +0100

    Home page displays the logged in users display name

--- foodwasteapp/src/pages/Home/Home.js
+++ foodwasteapp/src/pages/Home/Home.js
@@ -1,13 +1,14 @@
-import { Navbar, Nav, Container, Button } from "react-bootstrap"
-
+import { Button } from "react-bootstrap"
+import { useAuthContext } from "../../hooks/useAuthContext"
 
 export default function Home() {
-
+  const { user } = useAuthContext()
+  console.log(user.displayName)
 
   return (
     <div>
         <h1>Home Page</h1>
-        <h2>Hello </h2>
+        <h2>Hello, {user.displayName} </h2>
         <Button href="/Favourites">Go to Favourites</Button>
     </div>
   )
